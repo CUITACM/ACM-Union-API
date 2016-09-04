673961a3e032e46e116c683e030cdf6ee71ad376
Raynxxx <rayn1027@outlook.com>
2016-09-05 00:25:10 +0800

    create new model Submit

--- db/migrate/20160904050230_create_submits.rb
+++ db/migrate/20160904050230_create_submits.rb
@@ -1,7 +1,19 @@
 class CreateSubmits < ActiveRecord::Migration[5.0]
   def change
     create_table :submits do |t|
+      t.string :pro_id, limit: 32
+      t.string :run_id, limit: 32
+      t.integer :run_time
+      t.integer :memory
+      t.string :lang, limit: 32
+      t.string :result, limit: 32
+      t.text :code
+      t.datetime :submitted_at
+      t.integer :status
 
+      t.string :oj_name, limit: 32
+      t.integer :user_id
+      t.string :user_name
       t.timestamps
     end
   end
