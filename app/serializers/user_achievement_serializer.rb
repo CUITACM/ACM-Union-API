0d46e8682c7a474efcfceb86acf145338f56a650
Raynxxx <rayn1027@outlook.com>
2017-04-26 17:25:30 +0800

    fix achieve api

--- app/serializers/user_achievement_serializer.rb
+++ app/serializers/user_achievement_serializer.rb
@@ -1,8 +1,12 @@
 class UserAchievementSerializer < ActiveModel::Serializer
   attributes :id, :current, :total
-  attributes :completed, :completed_at, :situation, :score
+  attributes :completed_at, :situation, :score
   attributes :created_at, :updated_at
   attributes :user_id
+
+  attribute :completed do
+    object.completed.present? ? object.completed : false
+  end
 
   belongs_to :achievement
   
