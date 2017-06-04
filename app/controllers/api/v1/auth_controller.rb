0ec29aefa3358c3923973cf6139074470c62e1bc
Raynxxx <rayn1027@outlook.com>
2017-06-05 03:29:31 +0800

    update register

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -15,6 +15,9 @@ class Api::V1::AuthController < ApplicationController
       user = user_info.user if user_info.present?
     end
     if user.present? && user.authenticate(login_params[:password])
+      if user.status == User::STATUS_APPLY || user.status == User::STATUS_REJECT
+        render json: { error: 1, message: '非法用户' } and return
+      end
       expires = AcmUnionApi::TOKEN_EXPIRES
       render json: { token: user.token(expires), expire_time: expires }
     else
@@ -22,10 +25,33 @@ class Api::V1::AuthController < ApplicationController
     end
   end
 
+  def register
+    requires! :display_name
+    requires! :nickname
+    requires! :password
+    requires! :description
+
+    exist_user = User.find_by(nickname: register_params[:nickname])
+    if exist_user.present?
+      render json: { error: 1, messgae: '该用户名已存在' } and return
+    end
+
+    if User.create(register_params)
+      render json: { error: 0 }
+    else
+      error!({ error: 1, messgae: '注册失败' })
+    end
+
+  end
+
   private
 
   def login_params
     params.permit(:nickname, :password)
+  end
+
+  def register_params
+    params.permit(:display_name, :nickname, :password, :gender, :description)
   end
 
 end
