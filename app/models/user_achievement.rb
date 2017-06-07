0eaf4b669b0d5c26dbd4db7cabedc00357ede014
Sawyer <zzuu666@outlook.com>
2017-06-07 16:04:59 +0800

    fix user_achievement

--- app/models/user_achievement.rb
+++ app/models/user_achievement.rb
@@ -5,6 +5,10 @@ class UserAchievement < ApplicationRecord
   belongs_to :user
   belongs_to :achievement
 
+  before_create do
+    self.completed = false 
+  end
+
   def self.by_owner(achieve_id, user_id)
     self.find_or_initialize_by(
       achievement_id: achieve_id,
